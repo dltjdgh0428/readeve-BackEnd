5a08c3113cac5345d26796fe471b461950f280a7
dltjdgh0428 <dltjdgh0428@naver.com>
2024-04-01 04:54:32 +0900

    feat : JWT 기반 세션 인증 구현 완료

--- src/main/java/com/book_everywhere/jwt/token/JwtProvider.java
+++ src/main/java/com/book_everywhere/jwt/token/JwtProvider.java
@@ -56,7 +56,7 @@ public class JwtProvider {
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(24 * 60 * 60);
         //cookie.setSecure(true);
-        cookie.setPath("/");
+//        cookie.setPath("/");
         cookie.setHttpOnly(true);
 
         return cookie;
