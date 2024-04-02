ca1ab6aec0ed965303e243c93aa6d7b59473ab72
dltjdgh0428 <dltjdgh0428@naver.com>
2024-04-02 16:48:33 +0900

    fix : refresh Token 삭제

--- src/main/java/com/book_everywhere/jwt/filter/CustomSuccessHandler.java
+++ src/main/java/com/book_everywhere/jwt/filter/CustomSuccessHandler.java
@@ -1,16 +1,11 @@
 package com.book_everywhere.jwt.filter;
 
 import com.book_everywhere.auth.dto.CustomOAuth2User;
-import com.book_everywhere.auth.entity.Role;
-import com.book_everywhere.jwt.dto.RefreshDto;
-import com.book_everywhere.jwt.service.RefreshService;
 import com.book_everywhere.jwt.token.JwtProvider;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -21,15 +16,12 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 
-import static com.book_everywhere.jwt.token.TokenType.ACCESS;
-import static com.book_everywhere.jwt.token.TokenType.REFRESH;
 
 @RequiredArgsConstructor
 @Component
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JwtProvider jwtProvider;
-    private final RefreshService refreshService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -43,13 +35,9 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         GrantedAuthority auth = iterator.next();
         String role = auth.getAuthority();
 
-        String access = jwtProvider.createJwt(ACCESS.getType(), username, role, ACCESS.getExpirationTime());
-        String refresh = jwtProvider.createJwt(REFRESH.getType(), username, role, REFRESH.getExpirationTime());
-        refreshService.리프레시토큰생성(new RefreshDto(username, refresh, REFRESH.getExpirationTime()));
+        String token = jwtProvider.createJwt(username, role, 60*60*60*60L);
 
-        response.setHeader(ACCESS.getType(), access);
-        response.addCookie(jwtProvider.createCookie(ACCESS.getType(), access));
-        response.addCookie(jwtProvider.createCookie(REFRESH.getType(), refresh));
+        response.addCookie(jwtProvider.createCookie("Authorization", token));
         response.sendRedirect("https://www.bookeverywhere.site/");
         response.setStatus(HttpStatus.OK.value());
     }
--- src/main/java/com/book_everywhere/jwt/token/JwtProvider.java
+++ src/main/java/com/book_everywhere/jwt/token/JwtProvider.java
@@ -14,9 +14,6 @@ import java.util.Date;
 
 @Component
 public class JwtProvider {
-
-    private final Logger logger = LoggerFactory.getLogger(JwtProvider.class);
-
     private SecretKey secretKey;
 
     public JwtProvider(@Value("${app.jwt.secret}") String secret) {
@@ -31,17 +28,12 @@ public class JwtProvider {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
     }
 
-    public String getCategory(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
-    }
-
     public Boolean isExpired(String token) {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
     }
 
-    public String createJwt(String category, String username, String role, Long expiredMs) {
+    public String createJwt( String username, String role, Long expiredMs) {
         return Jwts.builder()
-                .claim("category", category)
                 .claim("username", username)
                 .claim("role", role)
                 .issuedAt(new Date(System.currentTimeMillis()))
@@ -58,27 +50,5 @@ public class JwtProvider {
         cookie.setHttpOnly(true);
 
         return cookie;
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parser()
-                    .setSigningKey(secretKey)
-                    .build()
-                    .parseClaimsJws(token);
-            return true;
-        } catch (ExpiredJwtException e) {
-            logger.info("만료된 JWT 토큰입니다.");
-        } catch (UnsupportedJwtException e) {
-            logger.info("지원되지 않는 JWT 토큰입니다.");
-        } catch (MalformedJwtException e) {
-            logger.info("잘못된 형식의 JWT 토큰입니다.");
-        } catch (SecurityException e) {
-            logger.info("잘못된 JWT 서명입니다.");
-        } catch (IllegalArgumentException e) {
-            logger.info("JWT 토큰이 잘못되었습니다.");
-        }
-        // 위의 예외 중 하나라도 발생했다면, 토큰이 유효하지 않음을 의미합니다.
-        return false;
     }
 }
