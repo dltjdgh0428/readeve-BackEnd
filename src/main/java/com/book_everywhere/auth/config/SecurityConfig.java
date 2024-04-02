f72a88c36956a290e3c68d58b8e9a2cbb220d121
dltjdgh0428 <dltjdgh0428@naver.com>
2024-04-02 18:26:38 +0900

    fix : 필터 위치 조정

--- src/main/java/com/book_everywhere/auth/config/SecurityConfig.java
+++ src/main/java/com/book_everywhere/auth/config/SecurityConfig.java
@@ -14,8 +14,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
