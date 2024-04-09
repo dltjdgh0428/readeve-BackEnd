3edaeab4b147932ac91abfa5ad00b012976126d4
dltjdgh0428 <dltjdgh0428@naver.com>
2024-04-09 22:29:44 +0900

    fix : @EnableRedisRepositories 제거

--- src/main/java/com/book_everywhere/common/redis/RedisConfig.java
+++ src/main/java/com/book_everywhere/common/redis/RedisConfig.java
@@ -12,12 +12,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
 @Configuration
-@EnableRedisRepositories
-//@EnableJpaRepositories(basePackages = "com.book_everywhere.common.auth.repository")
-//@EnableRedisRepositories(basePackages = "com.book_everywhere.common.cache.repository")
+//@EnableRedisRepositories
 public class RedisConfig {
 
-//    private static final Logger logger = LoggerFactory.getLogger(RedisConfig.class);
 
     @Value("${spring.redis.host}")
     private String redisHost;
@@ -25,19 +22,11 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
-//    @PostConstruct
-//    public void postConstruct() {
-//        logger.info("Redis Host: {}", redisHost);
-//        logger.info("Redis Port: {}", redisPort);
-//    }
-
-    // RedisProperties로 yaml에 저장한 host, post를 연결
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
-    // serializer 설정으로 redis-cli를 통해 직접 데이터를 조회할 수 있도록 설정
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
